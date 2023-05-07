eb2dfedf72c807db0e761e223f1614116619e969
Hamzah Fayad <hamzahfayad99@gmail.com>
2023-05-07 11:24:05 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import "./App.css";
+import {tiles_data} from "./data/tiles";
+import { useState, useEffect } from "react";
+
+function App() {
+
+  // shuffle array function
+  const shuffle = array => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+    return array;
+  }
+  
+  //state for each tile in tiles_data
+  const [tile, setTile] = useState(tiles_data);
+ 
+  //randomize each tile on page load
+  useEffect(() => {
+    const newTiles = shuffle([...tiles_data]);
+    setTile(newTiles);
+  }, []);
+
+  //on button click shuffle array
+  const handleShuffle = () => {
+    const newTiles = shuffle([...tile]);
+    setTile(newTiles);
+    console.log("Shuffle", tiles_data);
+  }
+  
+  return (
+    <div className="App Grid-width-wide">
+      <header className="App-header">
+        <h1>
+          mus
+          <span>A</span>ic
+        </h1>
+      </header>
+      <section className="tiles Grid-width-wide">
+        {
+          tile.map((t) => {
+            return(
+              <div className="tile" key={t.color}>{t.color}</div>
+            )
+          })
+        }
+      </section>
+      <button onClick={handleShuffle}>Generate</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data/tiles.js
@@ -0,0 +1,42 @@
+export const tiles_data = [
+  {
+    color: "red",
+    sound: "./public/audio/red.mp3",
+    shape: "circle",
+  },
+  {
+    color: "blue",
+    sound: "./public/audio/blue.mp3",
+    shape: "triangle",
+  },
+  {
+    color: "green",
+    sound: "./public/audio/blue.mp3",
+    shape: "triangle",
+  },
+  {
+    color: "yellow",
+    sound: "./public/audio/blue.mp3",
+    shape: "triangle",
+  },
+  {
+    color: "orange",
+    sound: "./public/audio/blue.mp3",
+    shape: "triangle",
+  },
+  {
+    color: "black",
+    sound: "./public/audio/blue.mp3",
+    shape: "triangle",
+  },
+  {
+    color: "white",
+    sound: "./public/audio/blue.mp3",
+    shape: "triangle",
+  },
+  {
+    color: "brown",
+    sound: "./public/audio/blue.mp3",
+    shape: "triangle",
+  },
+];
